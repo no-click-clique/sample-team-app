f5705281ff1fed23c6e137b61e65ca691aeadc20
christine <credhead89@gmail.com>
2020-03-09 14:01:24 -0400

    create new controller

---
+++ app/controllers/api/clones_controller.rb
@@ -0,0 +1,7 @@
+class Api::ClonesController < ApplicationController
+
+  def index
+    render json: {message: "Christine was here!"}
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   namespace :api do
     get "/pages" => "pages#index"
     get "/kittens" => "kittens#index"
+    get "/clones" => "clones#index"
   end
 end
